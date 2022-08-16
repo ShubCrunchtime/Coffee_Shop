473366af520c6c0993d74c0e0680cab80f580297
ShubCrunchtime <111334217+ShubCrunchtime@users.noreply.github.com>
2022-08-16 10:25:47 +0530

    Add files via upload

---
+++ Coffee_Shop.py
@@ -0,0 +1,17 @@
+x=input("What is your name? ")
+print("Welcome "+x+",good morning")
+print("What would you like to have?")
+Coffee=("$20")
+Tea=("$15")
+Breakfast=("$5")
+print("Coffee      $20")
+print("Tea         $15")
+print("Breakfast   $5")
+c=input("")
+if c==("Coffee"):
+  print("A "+c+" would be"+("$20"))
+if c==("Tea"):
+  print("A "+c+" would be"+("$15"))
+if c==("Breakfast"):
+  print("A "+c+" would be"+("$5"))
+
